c677e3bbfaa2b5c9bb68b3da4bb78073bc126ce5
Kyle Van Wagenen <kvanwagenen@gmail.com>
2015-03-31 17:38:29 -0600

    Don't inject assets into spree frontend or backend on install

--- lib/generators/sapphire_cms_spree/install/install_generator.rb
+++ lib/generators/sapphire_cms_spree/install/install_generator.rb
@@ -4,16 +4,6 @@ module SapphireCmsSpree
 
       class_option :auto_run_migrations, :type => :boolean, :default => false
 
-      def add_javascripts
-        append_file 'vendor/assets/javascripts/spree/frontend/all.js', "//= require spree/frontend/sapphire_cms_spree\n"
-        append_file 'vendor/assets/javascripts/spree/backend/all.js', "//= require spree/backend/sapphire_cms_spree\n"
-      end
-
-      def add_stylesheets
-        inject_into_file 'vendor/assets/stylesheets/spree/frontend/all.css', " *= require spree/frontend/sapphire_cms_spree\n", :before => /\*\//, :verbose => true
-        inject_into_file 'vendor/assets/stylesheets/spree/backend/all.css', " *= require spree/backend/sapphire_cms_spree\n", :before => /\*\//, :verbose => true
-      end
-
       def add_migrations
         run 'bundle exec rake railties:install:migrations FROM=sapphire_cms_spree'
       end
